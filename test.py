d3609ba1f214052d10c0840bfb506ed3033c8085
qianhao <362609254@qq.com>
2017-05-10 16:19:14 +0800

    add

---
+++ test.py
@@ -0,0 +1,10 @@
+class Caculator:
+    name = 'Good calculator'
+    price = 18
+    def add(self, x, y):
+        print(self.name)
+        result = x + y
+        print(result)
+    def minus(self, x, y):
+        result = x - y
+        print(result)
